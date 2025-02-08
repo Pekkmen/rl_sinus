d89c1ecfa2f52683bf0c818c029ec503d520b11f
Fekete J <129628497+Pekkmen@users.noreply.github.com>
2025-02-08 15:51:38 +0100

    Threshold lowered for the reward function.

--- rl_sinus.m
+++ rl_sinus.m
@@ -2,10 +2,10 @@ close all
 clear
 clc
 
-obs_info = rlNumericSpec([1 1], LowerLimit=0, UpperLimit=2*pi);
+obs_info = rlNumericSpec([1 1], LowerLimit=-pi, UpperLimit=pi);
 obs_info.Name = "Sinus Parameter";
 
-act_info = rlNumericSpec([1 1], LowerLimit=0, UpperLimit=2);
+act_info = rlNumericSpec([1 1], LowerLimit=-1, UpperLimit=1);
 act_info.Name = "Sinus Result";
 
 reset_fcn_handle = @()reset_train();
@@ -17,21 +17,23 @@ sinus_train_env = rlFunctionEnv( ...
 
 function [initial_observation, portfolio] = reset_train()
     portfolio = struct;
-    initial_observation = rand(1)*pi*2; %2*pi*rand(1)-pi; 
+    initial_observation = 2*pi*rand(1)-pi; %(rand(1)*pi*2); %2*pi*rand(1)-pi; 
     portfolio.LastValue = initial_observation;
 end
 
 function [next_observation, reward, is_done, portfolioOut] = step_train( ...
     action, portfolio)
-    expected_prediction = 1+sin(portfolio.LastValue);
+    expected_prediction = (sin(portfolio.LastValue));
     % reward = 1 / 100 / (0.01 + abs(action - expected_prediction));
     % reward = exp(-5*abs(action-expected_prediction));
-    if (abs(action-expected_prediction) > 0.03)
+    if (abs(action-expected_prediction) > 0.003)
         reward = -abs(action-expected_prediction);
+        %reward = -1;
     else 
         reward = 1;
     end
-    next_observation = rand(1)*pi*2; %2*pi*rand(1)-pi;
+    disp("EXP = " + expected_prediction + ", ACT = " + action + ", RWD = " + reward)
+    next_observation = 2*pi*rand(1)-pi;%(rand(1)*pi*2); %2*pi*rand(1)-pi;
     %portfolioOut = portfolio;
     portfolioOut = struct;
     portfolioOut.LastValue = next_observation;
--- rl_sinus_plot.m
+++ rl_sinus_plot.m
@@ -8,7 +8,7 @@ function rl_sinus_plot(agent, interval_start, interval_end)
     for i = 1:length(interval)
         expected_answer = getAction(agent, interval(i));
         expected_answers(i) = expected_answer{1};
-        %expected_answers(i) = sin(interval(i));
+        % expected_answers(i) = sin(interval(i));
         disp(interval(i) + " = " + gather(expected_answers(i)) + " (" + num2str(sin(interval(i)), '%.5f') + ")")
     end 
 
